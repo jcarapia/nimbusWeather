8758816839314e569f1d73297aff6e4845a74ddb
Jorge <jcarapia83@Gmail.com>
2017-03-02 19:16:37 -0800

    refactored weatherday

--- public/bundle.js
+++ public/bundle.js
@@ -24421,20 +24421,6 @@
 			return weather_class;
 		}(code);
 
-		// if(code >=5 && code <=8 || code >=13 && code <=16 || code >=41 && code <=43 || code === 46 ){
-		// 	weather_class='weather_icon snow';
-		// } else if (code >=0 && code <= 6 || code >= 37 && code <= 38 || code === 45 || code === 47) {
-		// 	weather_class='weather_icon thunder';
-		// } else if ( code >=9 && code <=12 || code === 17 || code ===18  || code === 35 || code === 39 || code === 40) {
-		// 	weather_class='weather_icon rain'; 
-		// } else if (code >= 27 && code <= 30 || code === 44) {
-		// 	weather_class='weather_icon partly_cloudy';
-		// } else if (code === 26) {
-		// 	weather_class='weather_icon cloudy';
-		// } else {
-		// 	weather_class='weather_icon sunny';
-		// };
-
 		return _react2.default.createElement(
 			'div',
 			{ className: 'weather_day_container col-lg-6 col-md-12' },
